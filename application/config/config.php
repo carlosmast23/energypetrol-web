e9108a523ca1b7856cbfaf4243be4bd4ddb29c8c
carlosmast23 <carlosmast2301@hotmail.es>
2019-07-22 13:20:43 -0500

    Agregada configuracion para el servidor real

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/runas/energypetrol/';
+$config['base_url'] = 'http://www.energypetrol.net/';
 
 /*
 |--------------------------------------------------------------------------

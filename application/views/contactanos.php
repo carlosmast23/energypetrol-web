9823aed0b2fca355e916364790472ff7005a9a4c
Suco <suco.pao@gmail.com>
2020-11-09 11:57:37 -0500

    mapa
    mapa tamanio

--- application/views/contactanos.php
+++ application/views/contactanos.php
@@ -1,6 +1,5 @@
-<div align="center">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7979.600143403408!2d-78.47254773091728!3d-0.1732911982551052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d59077096f5739%3A0x9f0117c8ecc3fca2!2sEnergypetrol%20S.%20A.!5e0!3m2!1ses!2sec!4v1604940804962!5m2!1ses!2sec" width="600" height="450" frameborder="0" style="border:0;text-align: center" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-</div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7979.600143403408!2d-78.47254773091728!3d-0.1732911982551052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d59077096f5739%3A0x9f0117c8ecc3fca2!2sEnergypetrol%20S.%20A.!5e0!3m2!1ses!2sec!4v1604940804962!5m2!1ses!2sec" width="100%" height="550" frameborder="0" style="border:0;text-align: center" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
 <section class="contact-section">
     <div class="container">
         <div class="col-md-4">

d1d4347d7c59d8641c217f35706b8b6d8e81d45e
Suco <suco.pao@gmail.com>
2021-05-03 14:58:32 -0500

    Update index.php

--- application/views/index.php
+++ application/views/index.php
@@ -6,27 +6,27 @@
 
 <script>
 
-    document.addEventListener('DOMContentLoaded', function () {
-
-        console.log("despues de carrgar");
-        var modal = document.getElementById("myModal");
-        modal.style.display = "block";
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function () {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-    }, false);
+//    document.addEventListener('DOMContentLoaded', function () {
+//
+//        console.log("despues de carrgar");
+//        var modal = document.getElementById("myModal");
+//        modal.style.display = "block";
+//
+//        // Get the <span> element that closes the modal
+//        var span = document.getElementsByClassName("close")[0];
+//
+//        // When the user clicks on <span> (x), close the modal
+//        span.onclick = function () {
+//            modal.style.display = "none";
+//        }
+//
+//        // When the user clicks anywhere outside of the modal, close it
+//        window.onclick = function (event) {
+//            if (event.target == modal) {
+//                modal.style.display = "none";
+//            }
+//        }
+//    }, false);
 
 
 </script>

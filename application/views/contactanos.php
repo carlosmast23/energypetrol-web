0295fe9f22adb02c2396c36485c6d6706c14e24a
Suco <suco.pao@gmail.com>
2021-08-19 11:08:00 -0500

    cambios de nombre
    cambios de nombre energypetrol

--- application/views/contactanos.php
+++ application/views/contactanos.php
@@ -5,7 +5,7 @@
             <div class="col-md-4">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3462.490549267942!2d-95.72520818448754!3d29.792369281974672!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640d89b338c5901%3A0x869d1fb2c1193159!2s1834%20Snake%20River%20Rd%2C%20Katy%2C%20TX%2077449%2C%20EE.%20UU.!5e0!3m2!1ses!2sec!4v1626389469589!5m2!1ses!2sec" width="100%" height="350" frameborder="0" style="border:0;text-align: center" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 <br/>
-                <h4 style="text-align: center">Energypetrol S.A.</h4>
+                <h4 style="text-align: center">Energypetrol</h4>
                 <h4 style="text-align: center">USA</h4>
                 <div class="contact-info" style="padding-left: 25%">
                     <ul class="information-list">
@@ -37,7 +37,7 @@
             <div class="col-md-4">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3327.1956836621894!2d-70.542417!3d-33.4962856!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662d193ba5747dd%3A0x5d96d35896a8ca68!2sAv.%20Las%20Perdices%204760%2C%20Penalolen%2C%20Pe%C3%B1alol%C3%A9n%2C%20Regi%C3%B3n%20Metropolitana%2C%20Chile!5e0!3m2!1ses!2sec!4v1626388362606!5m2!1ses!2sec" width="100%" height="350" frameborder="0" style="border:0;text-align: center" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>                
                 <br/>
-                <h4 style="text-align: center">Energypetrol S.A.</h4>
+                <h4 style="text-align: center">Energypetrol</h4>
                 <h4 style="text-align: center">Chile</h4>
                 <div class="contact-info" style="padding-left: 25%">
                     <ul class="information-list">

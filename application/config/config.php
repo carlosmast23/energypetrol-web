f02cd1858f1dcd9eef345ffb6f6573bbcb0f2cc3
Suco <suco.pao@gmail.com>
2022-08-18 08:06:21 -0500

    basePath
    basePath config

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-//$config['base_url'] = 'https://www.energypetrol.net/';
-$config['base_url'] = 'http://localhost/energypetrol-web/';
+$config['base_url'] = 'https://www.energypetrol.net/';
+//$config['base_url'] = 'http://localhost/energypetrol-web/';
 
 
 /*
